b17dbf135d7e421a700967f179a525255cc46bdc
calculusgyan <calculusgyan@gmail.com>
2018-12-21 02:28:58 +0000

    Create include_head.h
    header file for p_10660763.cc

---
+++ include_head.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_HEAD_H_INCLUDED
+#define INCLUDE_HEAD_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <iomanip>
+#include <time.h>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+#endif // INCLUDE_HEAD_H_INCLUDED
